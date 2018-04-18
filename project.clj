1dbcca1705eb36f17eec89c38127326e024bf959
Tim McCormack <cortex@brainonfire.net>
2018-04-18 08:47:19 -0400

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.timmc/lein-diff "0.2.0-SNAPSHOT"
+(defproject org.timmc/lein-diff "0.2.0"
   :description "Compute diffs between versions of a project"
   :url "https://github.com/timmc/lein-diff"
   :license {:name "Eclipse Public License"
