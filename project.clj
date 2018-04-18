70ed3f3b343c8ed6d8257bf3a6eb30df280636bd
Tim McCormack <cortex@brainonfire.net>
2018-04-17 22:57:02 -0400

    Bump version in project file too

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.timmc/lein-diff "0.1.2-SNAPSHOT"
+(defproject org.timmc/lein-diff "0.2.0-SNAPSHOT"
   :description "Compute diffs between versions of a project"
   :url "https://github.com/timmc/lein-diff"
   :license {:name "Eclipse Public License"
